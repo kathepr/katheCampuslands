640a17f38a69da038bf9346952eb036ebbdbec3e
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-27 07:00:23 -0500

    -

--- Ejercicio Clase - Tuplas.py
+++ Ejercicio Clase - Tuplas.py
@@ -17,8 +17,8 @@ colegio = input ("Ingrese el nombre del colegio al que perteneció: ")
 pregrado = input ("Ingrese sus estudios de pregrado: ")
 universidad_pre = input ("Ingrese la universidad en la que realizó sus estudios de pregrado: ")
 postgrado = input ("Ingrese sus estudios de postgrado: ")
-universidad_post = input ("Ingrese la universidad en la que realizó sus estudios de postgrado:")
-educacion = tuple (colegio, pregrado, universidad_pre, postgrado, universidad_post)
+universidad_post = input ("Ingrese la universidad en la que realizó sus estudios de postgrado: ")
+educacion = tuple ((colegio, pregrado, universidad_pre, postgrado, universidad_post))
 
 print (f"""Asistió al colegio {educacion[0]}. Estudió {educacion[1]} en la universidad {educacion[2]}.
        Tiene los siguientes estudios de post-grado {educacion[3]} y los cursó en la institución: {educacion[4]}""")
@@ -28,5 +28,5 @@ trabajo= input ("Ingrese el nombre de la empresa de su último empleo: ")
 cargo = input ("Ingrese el cargo que tenía en la empresa: ")
 laboral = tuple((trabajo, cargo))
 
-print (f"""Su último trabajo fue en {laboral[0]}, en el que ocuó el cargo de {cargo[1]}.""")
+print (f"""Su último trabajo fue en {laboral[0]}, en el que ocuó el cargo de {laboral[1]}.""")
 
