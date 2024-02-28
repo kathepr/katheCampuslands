07c0142fc403e42033ac1044fdb4f084ffea94b5
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-28 09:06:18 -0500

    -

--- CORRECCION - Diccionarios - Panadería.py
+++ CORRECCION - Diccionarios - Panadería.py
@@ -1,17 +1,17 @@
 #Crear diccionario de pan dulce con sus respectivos valores
 menu = { 
     "Salado":{
-        "Baguete": 4000,
-        "Croissant":2500,
-        "Pan Integral":6000,
-        "Pan de Yuca":1500,
-        "Pan de Bono":1200,
-        "Almojabana":1000,
-        "Pastel de Pollo":5000,
-        "Pan de ajo":2500,
-        "Palito de queso":800,
-        "Empanada":2000
-    }
+            "Baguete": 4000,
+            "Croissant":2500,
+            "Pan Integral":6000,
+            "Pan de Yuca":1500,
+            "Pan de Bono":1200,
+            "Almojabana":1000,
+            "Pastel de Pollo":5000,
+            "Pan de ajo":2500,
+            "Palito de queso":800,
+            "Empanada":2000
+            }
 }
 
 
@@ -26,5 +26,3 @@ print("\033[2m3. Bebidas (CATEGORÍA EN CONSTRUCCIÓN)\033[0m")
 
 print("\n")
 print ("Ingrese la categoría que desea consultar")
-
-
