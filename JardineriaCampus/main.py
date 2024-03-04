b9a6b09eec58ac6de5aea198518b0c7fd2f3b76b
kathepr <160674528+kathepr@users.noreply.github.com>
2024-03-04 09:08:49 -0500

    -

--- JardineriaCampus/main.py
+++ JardineriaCampus/main.py
@@ -1,3 +1,13 @@
-from modules.getClients import searchName
+from tabulate import tabulate
 
-searchName()
+import modules.getClients as cliente
+
+#El primer archivo, está importando el segundo y el segundo al tercero
+#De esta forma, tenemos acceso al contenido de los tres archivos. 
+
+#print(cliente.getAllClientName())
+#print(cliente.getOneClientCodigo(20))
+#print(cliente.getAllClientCreditCiudad(5000, "Humanes"))
+#print(cliente.getAllClientPaisRegionCiudad("France", None, "Paris"))
+
+print(tabulate(cliente.getAllClientPaisRegionCiudad("Australia"),tablefmt="grid"))
--- JardineriaCampus/modules/getClients.py
+++ JardineriaCampus/modules/getClients.py
@@ -1,6 +1,50 @@
-from storage.cliente import clientes 
+import storage.cliente as cli #cli = abreviación de cliente  
 
 
 
-def searchName():
-    print (clientes)
+#Importar el archivo cliente que está en la carpeta storage
+#as ayuda a ponerle un alias.
+
+# PARA MOSTRAR CLIENTES:
+#print(cli.clientes)
+#Se coloca el alias del archivo y el nombre del dato que se quiere imprimir
+
+def getAllClientName():
+    clienteName = []
+    for val in cli.clientes:
+        codigoName = dict({
+            "codigo_cliente": val.get('codigo_cliente'),
+            "nombre_cliente": val.get('nombre_cliente')
+        })
+        clienteName.append(codigoName)
+    return clienteName
+
+def getOneClientCodigo(codigo):
+    clienteName = []
+    for val in cli.clientes:
+        if(val.get('codigo_cliente')== codigo):
+            return {
+                "codigo_cliente": val.get('codigo_cliente'),
+                "nombre_cliente": val.get('nombre_cliente')
+            }
+
+def getAllClientCreditCiudad(limiteCredit, ciudad):
+    clientCredit = list()
+    for val in cli.clientes:
+        if(val.get('limite_credito') >= limiteCredit and val.get('ciudad')==ciudad):
+            clientCredit.append(val)
+    return clientCredit
+
+
+
+#ARREGLAR ESTA PARTE DEL CÓDIGO - OJO: 
+def getAllClientPaisRegionCiudad(pais, region=None, ciudad=None):
+    clientZone = list()
+    for val in cli.clientes:
+        if(
+            val.get('pais') == pais and
+            (val.get('region') == region and val.get('region') == None) or
+            (val.get('ciudad') == ciudad and val.get('ciudad') == None)
+        ):
+            clientZone.append(val)
+    return clientZone
