09ce11f7ab984c1ca6649c07599b2d243867800a
kathepr <160674528+kathepr@users.noreply.github.com>
2024-03-05 07:55:16 -0500

    -

--- examenFundamentos/Area Triangulo.py
+++
@@ -1,8 +0,0 @@
-lado = int(input("Ingrese la medida de la base: "))
-altura = int(input("Ingrese la medida de la altura: "))
-
-area = (altura * lado)//2
-
-if area > 1000:
-    print("DATOS NO VALIDOS")
-
--- examenFundamentos/Metros a Kilometros.py
+++
@@ -1,5 +0,0 @@
-metros = int(input("\nIngrese una distancia en metros: "))
-
-kilometros = metros * 0.001
-
-print(f"\nCONVERSIÓN: {metros} metros equivalen a {kilometros} kilometros.")
