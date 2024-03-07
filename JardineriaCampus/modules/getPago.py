f38a9b165f8242f7a9829c569896765d36c4ba4b
kathepr <160674528+kathepr@users.noreply.github.com>
2024-03-06 19:17:29 -0500

    -

--- JardineriaCampus/modules/getPago.py
+++ JardineriaCampus/modules/getPago.py
@@ -1,13 +1,17 @@
 import storage.pago as pay   
 
-#TOCA CORREGIRLO, PARA QUE LEA LOS PRIMEROS 4 ELEMENTOS DEL AÑO
+
 def getAllPagoYear ():
     codigoPagoYear = []
+    codigoVisto = set() # set se utiliza para crear un conjunto, que es una colección desordenada y sin elementos duplicados
     for val in pay.pago:
-        if val.get("fecha_pago") == "2008":
+        year = val.get("fecha_pago")[:4] # Se selecciona los primeros 4 caracteres, que corresponden al año
+        codigo = val.get("codigo_cliente")
+        if year == "2008" and codigo not in codigoVisto:
             codigoPagoYear.append ({
-            "codigo_cliente": val.get("codigo_cliente"),
+            "codigo_cliente": codigo,
             "fecha_pago": val.get("fecha_pago")
-            })
+        })
+        codigoVisto.add(codigo) #add se usa con conjuntos set
         
     return codigoPagoYear
