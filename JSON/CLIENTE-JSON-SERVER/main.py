5bd78d6404c8322319fa96c83f8ac7f05d6f9de3
kathepr <160674528+kathepr@users.noreply.github.com>
2024-03-13 07:05:43 -0500

    -

--- JSON/CLIENTE-JSON-SERVER/main.py
+++ JSON/CLIENTE-JSON-SERVER/main.py
@@ -1,5 +1,5 @@
 
-import json 
+import json
 import requests
 
 #CON ESTO ESTAMOS tratando de obtener recursos (datos, archivos, etc.) de esa dirección web
@@ -7,20 +7,37 @@ import requests
 #print(obtener.json())
 
 
-
-
 #CON ESTO GUARDAMOS DATOS NUEVOS EN LA BASE DE DATOS:
-# empleadoNuevo = {
-#     "codigo": 888,
-#     "nombre": "Monica",
-#     "apellido": "Ruiz"
-# }
-# obtener = requests.post("http://172.16.100.114:5006", data=json.dumps(empleadoNuevo))
-# print(obtener)
+empleadoNuevo = {
+    "codigo": 888,
+    "nombre": "Monica",
+    "apellido": "Ruiz"
+}
+obtener = requests.post("http://172.16.100.114:5507", data=json.dumps(empleadoNuevo))
+print(obtener)
 
 
 
 
 #CON ESTO ELIMINAMOS DATOS:  AL FINAL COLOCAMOS UN SLASH Y EL NÚMERO ID DEL EMPLEADO QUE QUEREMOS QUITAR
-eliminar = requests.delete("http://172.16.100.114:5006/4")
-print(eliminar)
+# eliminar = requests.delete("http://172.16.100.114:5006/4")
+# print(eliminar)
+
+
+#DECODIFICANDO EN JSON
+# plantilla = '[{"nombre": "Miguel"}, {"nombre": "Jhon"}, ["Hola mundo"]]'
+# platilla = json.loads(plantilla)  # Decodificar 
+# print(platilla[1].get("nombre"))
+
+
+
+
+
+# example = list([
+#     dict({"nombre": "Miguel"}),
+#     dict({"nombre": "Jhon"}),
+#     list(["Hola mundo"])
+# ])
+
+# example = json.dumps(example, indent = 4)
+# print(example)
