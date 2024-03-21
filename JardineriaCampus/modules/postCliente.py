6b7111522e42040a3e6976511d4ba61ff7fc3a6f
kathepr <160674528+kathepr@users.noreply.github.com>
2024-03-21 13:04:56 -0500

    Validacion post y update cliente

--- JardineriaCampus/modules/postCliente.py
+++ JardineriaCampus/modules/postCliente.py
@@ -1,26 +1,78 @@
 import json
 import requests
 from tabulate import tabulate
+import re
+
+
+def validar_input(patron, mensaje):
+    while True: 
+        entrada = input(mensaje)
+        if patron.match(entrada):
+            confirmacion = input(f"¿Confirma el ingreso de este dato '{entrada}'? (S/N) ").strip().lower()
+            if confirmacion == "s":
+                return entrada
+        else:
+            print("El dato no cumple con los parametros establecidos. Vuelva a intentarlo.")
+
+
+
 
 
 
 def postCliente():
+
+
+#Expresiones regulares para cada dato:
+    codigoEmpleadoR = re.compile(r'^[0-9]+$')
+    nombreClienteR = re.compile(r'^[A-Za-z\s]+$')
+    nombreContactoR = re.compile(r'^[A-Za-z\s]+$')
+    apellidoContactoR = re.compile(r'^[A-Za-z]+$')
+    telefonoR = re.compile(r'^\d+(\s*\d+)*$')
+    faxR = re.compile(r'^\d+(\s*\d+)*$')
+    direccion1R = re.compile(r'^[^\n]+$')
+    direccion2R = re.compile(r'^[^\n]+$')
+    ciudadR = re.compile(r'^[A-Za-z\s]+$') 
+    regionR = re.compile(r'^[A-Za-z\s]+$')
+    paisR = re.compile(r'^[A-Za-z\s]+$')
+    codigoPostalR = re.compile(r'^[0-9]+$')
+    codigoRepVentasR = re.compile(r'^[0-9]+$')
+    limiteCreditoR = re.compile(r'^[0-9]+(\.[0-9]+)?$')
+
+#Obtener los datos del usuario: 
+    codigo_cliente = int(validar_input(codigoEmpleadoR, "Ingrese el código del cliente: "))
+    nombre_cliente = validar_input(nombreClienteR, "Ingrese el nombre del cliente: ")
+    nombre_contacto = validar_input(nombreContactoR, "Ingrese el nombre del contacto: ")
+    apellido_contacto = validar_input(apellidoContactoR, "Ingrese el apellido del contacto: ")
+    telefono = validar_input(telefonoR, "Ingrese el número de telefono: ")
+    fax = validar_input(faxR, "Ingrese el número de fax: ")
+    direccion1 = validar_input(direccion1R, "Ingrese la dirección 1: ")
+    direccion2 = validar_input(direccion2R, "Ingrese la dirección 2: ")
+    ciudad = validar_input(ciudadR, "Ingrese la ciudad: ")
+    region = validar_input(regionR, "Ingrese la región: ")
+    pais = validar_input(paisR, "Ingrese el pais: ")
+    codigo_postal = validar_input(codigoPostalR, "Ingrese el código postal: ")
+    codigo_repVentas = int(validar_input(codigoRepVentasR, "Ingrese el código del representante de ventas: "))
+    limite_credito = int(validar_input(limiteCreditoR, "Ingrese el limite de crédito: "))
+
+
+
     cliente = {
-        "codigo_cliente": int(input("Ingrese el código del cliente: ")),
-        "nombre_cliente": input("Ingrese el nombre del cliente: "),
-        "nombre_contacto": input("Ingrese el nombre del contacto: "),
-        "apellido_contacto": input("Ingrese el apellido del contacto: "),
-        "telefono": input("Ingrese el número de telefono: "),
-        "fax": input("Ingrese el número de fax: "),
-        "linea_direccion1": input("Ingrese la dirección 1: "),
-        "linea_direccion2": input("Ingrese la dirección 1: "),
-        "ciudad": input("Ingrese la ciudad: "),
-        "region": input("Ingrese la región: "),
-        "pais": input("Ingrese el pais: "),
-        "codigo_postal": input("Ingrese el código postal: "),
-        "codigo_empleado_rep_ventas": int(input("Ingrese el código del representante de ventas: ")),
-        "limite_credito": int(input("Ingrese el limite de crédito: "))
+        "codigo_cliente": codigo_cliente,
+        "nombre_cliente": nombre_cliente,
+        "nombre_contacto": nombre_contacto,
+        "apellido_contacto": apellido_contacto,
+        "telefono": telefono,
+        "fax": fax,
+        "linea_direccion1": direccion1,
+        "linea_direccion2": direccion2,
+        "ciudad": ciudad,
+        "region": region,
+        "pais": pais,
+        "codigo_postal": codigo_postal,
+        "codigo_empleado_rep_ventas": codigo_repVentas,
+        "limite_credito": limite_credito
     }
+    
     url = "http://154.38.171.54:5001/cliente"
     data = json.dumps(cliente)
     peticion = requests.post(url,data)
@@ -34,22 +86,60 @@ def deleteCliente(id):
 
 
 def updateCliente(id):
+
+
+#Expresiones regulares para cada dato:
+    codigoEmpleadoR = re.compile(r'^[0-9]+$')
+    nombreClienteR = re.compile(r'^[A-Za-z\s]+$')
+    nombreContactoR = re.compile(r'^[A-Za-z\s]+$')
+    apellidoContactoR = re.compile(r'^[A-Za-z]+$')
+    telefonoR = re.compile(r'^\d+(\s*\d+)*$')
+    faxR = re.compile(r'^\d+(\s*\d+)*$')
+    direccion1R = re.compile(r'^[^\n]+$')
+    direccion2R = re.compile(r'^[^\n]+$')
+    ciudadR = re.compile(r'^[A-Za-z\s]+$') 
+    regionR = re.compile(r'^[A-Za-z\s]+$')
+    paisR = re.compile(r'^[A-Za-z\s]+$')
+    codigoPostalR = re.compile(r'^[0-9]+$')
+    codigoRepVentasR = re.compile(r'^[0-9]+$')
+    limiteCreditoR = re.compile(r'^[0-9]+(\.[0-9]+)?$')
+
+#Obtener los datos del usuario: 
+    codigo_cliente = int(validar_input(codigoEmpleadoR, "Ingrese el código del cliente: "))
+    nombre_cliente = validar_input(nombreClienteR, "Ingrese el nombre del cliente: ")
+    nombre_contacto = validar_input(nombreContactoR, "Ingrese el nombre del contacto: ")
+    apellido_contacto = validar_input(apellidoContactoR, "Ingrese el apellido del contacto: ")
+    telefono = validar_input(telefonoR, "Ingrese el número de telefono: ")
+    fax = validar_input(faxR, "Ingrese el número de fax: ")
+    direccion1 = validar_input(direccion1R, "Ingrese la dirección 1: ")
+    direccion2 = validar_input(direccion2R, "Ingrese la dirección 2: ")
+    ciudad = validar_input(ciudadR, "Ingrese la ciudad: ")
+    region = validar_input(regionR, "Ingrese la región: ")
+    pais = validar_input(paisR, "Ingrese el pais: ")
+    codigo_postal = validar_input(codigoPostalR, "Ingrese el código postal: ")
+    codigo_repVentas = int(validar_input(codigoRepVentasR, "Ingrese el código del representante de ventas: "))
+    limite_credito = int(validar_input(limiteCreditoR, "Ingrese el limite de crédito: "))
+
+
+
+
     clienteActualizado = {
-        "codigo_cliente": int(input("Ingrese el código del cliente: ")),
-        "nombre_cliente": input("Ingrese el nombre del cliente: "),
-        "nombre_contacto": input("Ingrese el nombre del contacto: "),
-        "apellido_contacto": input("Ingrese el apellido del contacto: "),
-        "telefono": input("Ingrese el número de telefono: "),
-        "fax": input("Ingrese el número de fax: "),
-        "linea_direccion1": input("Ingrese la dirección 1: "),
-        "linea_direccion2": input("Ingrese la dirección 1: "),
-        "ciudad": input("Ingrese la ciudad: "),
-        "region": input("Ingrese la región: "),
-        "pais": input("Ingrese el pais: "),
-        "codigo_postal": input("Ingrese el código postal: "),
-        "codigo_empleado_rep_ventas": int(input("Ingrese el código del representante de ventas: ")),
-        "limite_credito": int(input("Ingrese el limite de crédito: "))
+        "codigo_cliente": codigo_cliente,
+        "nombre_cliente": nombre_cliente,
+        "nombre_contacto": nombre_contacto,
+        "apellido_contacto": apellido_contacto,
+        "telefono": telefono,
+        "fax": fax,
+        "linea_direccion1": direccion1,
+        "linea_direccion2": direccion2,
+        "ciudad": ciudad,
+        "region": region,
+        "pais": pais,
+        "codigo_postal": codigo_postal,
+        "codigo_empleado_rep_ventas": codigo_repVentas,
+        "limite_credito": limite_credito
     }
+
     url = (f"http://154.38.171.54:5001/cliente/{id}")
     data = json.dumps(clienteActualizado)
     peticion = requests.put(url, data)
@@ -62,8 +152,9 @@ def updateCliente(id):
 def menu():
     while True:
         print("""
-        
-        ADMINISTRAR DATOS DE CLIENTES:
+        **************************************
+            ADMINISTRAR DATOS DE CLIENTES:
+        **************************************
 
         1. Guardar cliente nuevo
         2. Eliminar un cliente
